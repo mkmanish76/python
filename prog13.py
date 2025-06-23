b67fbcd59dcac5c034ef395372cc27b147728728
Manish Kumar <manish98uuu@gmail.com>
2025-06-24 00:09:10 +0530

    WAP to create Diagonal star pattern.

---
+++ prog13.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a nos"))
+m=int(input("Enter a nos"))
+for i in range(1,n):
+    print(" ")
+    for j in range(1,m):
+        if(i==j):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
