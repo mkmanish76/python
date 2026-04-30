75758b50f1fa28247930bab09693c3ae69197360
Manish Kumar <manish98uuu@gmail.com>
2026-04-30 23:45:29 +0530

    Relational operators.

--- prog06.py
+++ prog06.py
@@ -1,16 +1,9 @@
-#WAP to check a given no. is armstrong or not ?
-n=int(input("Enter any no.:"))
-sum=0
-digit=len(str(n))
-print("No. of digits=",digit) 
-t=n
-while(t>0):
-    sum=sum+pow(t%10,digit) # or *digit
-    t=t//10
-if(sum==n):
-        print("No. is armstrong")
-else:
-        print("this no. is not armstrong")
-        
-
-
+#Relational operators 
+a=20
+b=15
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a <=b)
+print(a < b)
+print(a > b)
